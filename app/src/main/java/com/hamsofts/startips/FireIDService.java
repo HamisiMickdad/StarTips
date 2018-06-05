4601fab82e4f25202b69b4073d840f66c5eda502
Hamisi Mickdad Aross <mhaross@student.maseno.ac.ke>
2018-06-05 19:11:40 +0300

    Initial commit

---
+++ app/src/main/java/com/hamsofts/startips/FireIDService.java
@@ -0,0 +1,10 @@
+package com.hamsofts.startips;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FireIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+
+    }
+}
